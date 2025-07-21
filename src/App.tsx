940c7ae067e30d50b707d1e01be5b71f9a03d969
natsuokawai <kawai.natsuo@gmail.com>
2025-07-21 12:07:39 +0900

    Initialize TypeScript, React and ECharts

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import LineChart from './components/LineChart';
+
+function App() {
+  return (
+    <div>
+      <h1>ECharts React Sample</h1>
+      <LineChart />
+    </div>
+  );
+}
+
+export default App;
